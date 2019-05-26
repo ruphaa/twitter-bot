1eba89223c1769f48e1e45dcab5afa8046b1ca64
ruphaafd <ruphaa.ganesh@freshworks.com>
2019-05-26 11:30:57 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+
+const Twitter = require('Twitter');
+const Airtable = require('airtable');
+const base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(process.env.AIRTABLE_BASE_KEY);
+
+const T = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_TOKEN,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+var today = new Date();
+var date = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
+
+  let promise = new Promise((resolve, reject) => {
+    base('Devtips').select({
+      filterByFormula: `date = "${date}" `,
+    }).eachPage(function page(records, fetchNextPage) {
+      records.forEach(function(record) {
+        resolve(record)
+      });
+    }, function done(error) {
+      console.log(error)
+    });
+  })
+  
+  promise.then(function(record) {
+    if(!(record.used == "true")) {
+      let id = record.id;
+      let message = record.fields.message;
+      let article = record.fields.article;
+      let tag = record.fields.tag.split(',').join('');
+      let tweet = `${message} ${article} ${tag}`;
+      T.post('statuses/update', {status: tweet}, function(error, tweet, response) {
+        if (!error) {
+          console.log(tweet);
+          base('Devtips').update(id, {
+            "used": "true"
+          })
+        }
+      });
+    }
+  })
+  .catch(function(error) {
+    console.log('Error-->', error)
+  })
